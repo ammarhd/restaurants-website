3c082ce44fa918da1a8f31d280043aabdb8665bd
Ammar <ammar.h.d@hotmail.com>
2021-02-13 22:07:25 +0100

    sitting the header

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/header";
 import Restaurants from "./components/resturants";
+import FavoriteRes from "./components/favoriteRes";
 
 function App() {
-  <Router>
-    <div className="container-fluid">
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Restaurants />
-        </Route>
-        <Route path="/FavoriteRes">
-          <FavoriteRes />
-        </Route>
-      </Switch>
-    </div>
-  </Router>;
+  return (
+    <Router>
+      <div className="container-fluid">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Restaurants />
+          </Route>
+          <Route path="/FavoriteRes">
+            <FavoriteRes />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>LOVE WHAT YOU EAT</h1>
+    </div>
+  );
+}
+
+export default Header;
