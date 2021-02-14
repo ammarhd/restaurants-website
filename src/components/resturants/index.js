c152457b0e94c94d0a4986ef3b1bc9b756caba0d
Ammar <ammar.h.d@hotmail.com>
2021-02-14 21:19:26 +0100

    sitting redux anf fetching the data from the API

--- src/components/resturants/index.js
+++ src/components/resturants/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchRestaurants,
+  restaurantsSelector,
+} from "../../slices/restaurants";
 
 function Resturants() {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const { restaurants, loading, hasError } = useSelector(restaurantsSelector);
+
+  // dispatch thunk when component first mounts
+  useEffect(() => {
+    dispatch(fetchRestaurants());
+  }, [dispatch]);
+
+  // error handling & map successful quary data
+  const renderRestaurants = () => {
+    if (loading) return <p>Loading Restaurants...</p>;
+    if (hasError) return <p>Cannot display Restaurants...</p>;
+
+    return restaurants.map((restaurant) => (
+      <div key={restaurant.id} className="title">
+        <h2>{restaurant.name.a}</h2>
+        <img src={restaurant.photo} alt="" />
+      </div>
+    ));
+  };
+
+  //console.log("restaurants : ", restaurants);
+  return (
+    <div>
+      <div>{renderRestaurants()}</div>
+    </div>
+  );
 }
 
 export default Resturants;
